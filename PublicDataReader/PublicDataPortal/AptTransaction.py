0b0886b35472f1e2baa94a77842a65975e915647
Wooil Jeong <mcwooil2@gmail.com>
2019-12-03 13:30:58 +0900

    1203

--- PublicDataReader/PublicDataPortal/AptTransaction.py
+++ PublicDataReader/PublicDataPortal/AptTransaction.py
@@ -12,6 +12,7 @@ class AptTransactionReader:
         self.serviceKey = serviceKey
 
         # 지역 코드 초기화
+        # 법정동 코드 출처 : https://code.go.kr
         path_code = "https://raw.githubusercontent.com/WooilJeong/PublicDataReader/f14e4de3410cc0f798a83ee5934070d651cbd67b/docs/%EB%B2%95%EC%A0%95%EB%8F%99%EC%BD%94%EB%93%9C%20%EC%A0%84%EC%B2%B4%EC%9E%90%EB%A3%8C.txt"
         code = pd.read_csv(path_code, encoding='cp949', sep='\t')
         code = code.loc[code['폐지여부']=='존재']
