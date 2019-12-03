38b6a82f252c5e41d9d53885c2bf4a12ebbcd7c7
Wooil Jeong <mcwooil2@gmail.com>
2019-12-03 10:44:07 +0900

    1203

--- PublicDataReader/__init__.py
+++ PublicDataReader/__init__.py
@@ -1,4 +1,4 @@
 from .data import AptTransactionReader
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __all__=['__version__', 'AptTransactionReader']
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="PublicDataReader",
-    version="0.0.3",
+    version="0.0.4",
     license='MIT',
     author="Wooil Jeong",
     author_email="wooil@kakao.com",
