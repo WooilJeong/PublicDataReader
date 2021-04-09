abb2460ec228c163e6f9fc55961b2fe1e43f04e0
Wooil Jeong <wooil@kakao.com>
2021-04-09 11:16:04 +0900

    fix readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="PublicDataReader",
-    version="2021.4.9",
+    version="2021.4.9.1",
     license="MIT",
     author="Wooil Jeong",
     author_email="wooil@kakao.com",
