9d44d1cf2f80a2fe74ad4e8bb0d0cb277151ae1a
정우일 <wooil@kakao.com>
2022-10-21 11:35:43 +0900

    fix

--- PublicDataReader/kosis/kosis.py
+++ PublicDataReader/kosis/kosis.py
@@ -7,7 +7,25 @@ import pandas as pd
 
 
 class Kosis:
-    """Kosis 공유서비스 클래스"""
+    """KOSIS 공유서비스 클래스
+
+    KOSIS 공유서비스에서 발급받은 사용자 인증키를 입력받아 초기화합니다.
+
+    Parameters
+    ----------
+    apiKey : str
+        KOSIS 공유서비스에서 발급받은 사용자 인증키
+    serviceName : str
+        KOSIS 공유서비스 서비스명
+
+    Examples
+    --------
+    >>> import PublicDataReader as pdr
+    >>> # KOSIS 공유서비스 OPEN API 인스턴스 생성
+    >>> apiKey = "YOUR_API_KEY"
+    >>> serviceName = "KOSIS통합검색" # (예시) KOSIS통합검색, 통계설명, 통계표설명, 통계목록, 통계자료
+    >>> kosis = pdr.Kosis(apiKey, serviceName)
+    """
 
     def __init__(self, apiKey, serviceName):
         self.apiKey = apiKey
@@ -22,6 +40,32 @@ class Kosis:
 
     def get_data(self, **kwargs):
         """API 호출
+
+        KOSIS 공유서비스 API를 호출하여 데이터를 반환합니다.
+
+        Parameters
+        ----------
+        **kwargs : dict
+            API 호출에 필요한 파라미터
+
+        Returns
+        -------
+        DataFrame
+            API 호출 결과를 DataFrame 형태로 반환합니다.
+
+        Examples
+        --------
+        >>> import PublicDataReader as pdr
+        >>> # KOSIS 공유서비스 OPEN API 인스턴스 생성
+        >>> apiKey = "YOUR_API_KEY"
+        >>> serviceName = "KOSIS통합검색"
+        >>> kosis = pdr.Kosis(apiKey, serviceName)
+        >>> # 파라미터 설정
+        >>> orgId = "101"
+        >>> tblId = "DT_1B040A3"
+        >>> metaItm = "ALL"
+        >>> # 데이터 조회
+        >>> df = kosis.get_data(orgId=orgId, tblId=tblId, metaItm=metaItm)
         """
         try:
             kwargs["apiKey"] = self.apiKey
